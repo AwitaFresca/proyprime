bd015ba714be1998ec3a3f9a2fe9961f72882698
AwitaFresca <maximelihual@gmail.com>
2021-07-28 07:09:14 -0300

    Entrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { InstrumentosComponent } from './paginas/instrumentos/instrumentos.component';
+import { MiembrosComponent } from './paginas/miembros/miembros.component';
+import { TiendasComponent } from './paginas/tiendas/tiendas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:"inicio", component:InicioComponent
+  },
+  {
+    path:"instrumentos", component:InstrumentosComponent
+  },
+  {
+    path:"tiendas", component:TiendasComponent
+  },
+  {
+    path:"miembros", component:MiembrosComponent
+  },
+  {
+    path:"contacto", component:ContactoComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {ButtonModule} from 'primeng/button';
+import {MenubarModule} from 'primeng/menubar';
+import {CardModule} from 'primeng/card';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { InstrumentosComponent } from './paginas/instrumentos/instrumentos.component';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
+import { MiembrosComponent } from './paginas/miembros/miembros.component';
+import { TiendasComponent } from './paginas/tiendas/tiendas.component';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    InstrumentosComponent,
+    ContactoComponent,
+    MiembrosComponent,
+    TiendasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    MenubarModule,
+    CardModule,
+
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
